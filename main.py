3e46cc222a4ac18a87d962944942e9b1393248cd
teambestas <134722296+teambestas@users.noreply.github.com>
2023-05-26 19:08:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -29,6 +29,32 @@ def start_flask_app():
     # Thread'i başlat
     t.start()
 
+from flask import Flask
+import threading
+import os
+import random
+
+import schedule
+import time
+from os import system
+def run_job():
+ system("main.py")
+
+# işlemi her 2 saatte bir çalıştırmak için
+schedule.every(2).hours.do(run_job)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
+  
+    
+
+
+
+ 
+
+
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(name)s - [%(levelname)s] - %(message)s'
